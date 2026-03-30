e390139d83cdea9cd60fcd1eb9c5606c02a31fce
LxDemar <el.conde.valdemar12@gmail.com>
2026-03-30 11:34:22 -0400

    Practica 1 terminada finalfinal

--- Practica1/12.js
+++ Practica1/12.js
@@ -7,29 +7,34 @@ Fecha: 30/03/2026
 Ejercicio 12
 Proporcione un ejemplo concreto donde el anidamiento de callbacks se puede reescribir mejor con async/await haciendo el código más limpio y mantenible
 */
-function paso1(callback){
-    setTimeout(() => {
-        console.log("Paso 1");
-        callback(5);
-    }, 1000);
-}
-function paso2(num, callback){
-    setTimeout(() => {
-        console.log("Paso 2");
-        callback(num * 2);
-    }, 1000);
-}
-function paso3(num, callback){
-    setTimeout(() => {
-        console.log("Paso 3");
-        callback(num + 10);
-    }, 1000);
-}
-
-paso1((res1) => {
-    paso2(res1, (res2) => {
-        paso3(res2, (res3) => {
-            console.log("Resultado:", res3);
-        });
+function paso1(){
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Paso 1");
+            resolve(5);
+        }, 1000);
     });
-});
+}
+function paso2(num){
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Paso 2");
+            resolve(num * 2);
+        }, 1000);
+    });
+}
+function paso3(num){
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Paso 3");
+            resolve(num + 10);
+        }, 1000);
+    });
+}
+async function ejecutar(){
+    const r1 = await paso1();
+    const r2 = await paso2(r1);
+    const r3 = await paso3(r2);
+    console.log("Resultado:", r3);
+}
+ejecutar();
